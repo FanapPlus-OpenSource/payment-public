fbeba3336c62db17c4b8b6217bab6707b922048b
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2017-07-07 21:40:17 +0430

    Initial commit

---
+++ v1.0.0/RSA Generator/src/Appson.Payment.KeyGenerator/Appson.Security.KeyGenerator.Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Appson.Security.KeyGenerator.Core;
+
+namespace Appson.Security.KeyGenerator.Console
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			do
+			{
+				CreateSignatureKey(GetDestinationDirectory());
+			} while (Continue());
+		}
+
+		private static void CreateSignatureKey(string baseAddress)
+		{
+			var signatureInformation = Generator.Generate();
+
+
+			File.WriteAllText(Path.Combine(baseAddress, "private.xml"), signatureInformation.PrivateXml);
+			File.WriteAllText(Path.Combine(baseAddress, "public.xml"), signatureInformation.PublicXml);
+			File.WriteAllText(Path.Combine(baseAddress, "private.rsa"), signatureInformation.PrivatePem);
+			File.WriteAllText(Path.Combine(baseAddress, "public.rsa"), signatureInformation.PublicPem);
+			System.Console.WriteLine($"Keys generated successfully at the following address\r\n{baseAddress}");
+
+			Process.Start("explorer.exe", baseAddress);
+		}
+
+		private static bool Continue()
+		{
+			System.Console.WriteLine("Continue (y/N)?");
+			var input = System.Console.ReadLine();
+			return input != null && string.Equals(input, "y", StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		private static string GetDestinationDirectory()
+		{
+			var flag = true;
+			var path = "";
+			do
+			{
+				try
+				{
+					System.Console.Write("Enter destination directory: ");
+					path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, System.Console.ReadLine() ?? "");
+					if (!Directory.Exists(path))
+					{
+						Directory.CreateDirectory(path);
+					}
+					flag = false;
+					return path;
+				}
+				catch (Exception ex)
+				{
+					System.Console.WriteLine(ex.Message);
+					System.Console.WriteLine("Directory path not valid");
+				}
+			} while (flag);
+			return path;
+		}
+	}
+}
---
+++ v1.0.0/RSA Generator/src/Appson.Payment.KeyGenerator/Appson.Security.KeyGenerator.Core/Generator.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+
+namespace Appson.Security.KeyGenerator.Core
+{
+	public static class Generator
+	{
+		public static SignatureInformation Generate()
+		{
+			var keyPairGenerator = new RsaKeyPairGenerator();
+			var parameters = new KeyGenerationParameters(new SecureRandom(), 2048);
+			keyPairGenerator.Init(parameters);
+
+			var keyPair = keyPairGenerator.GenerateKeyPair();
+
+			var privatePem = GetPrivateKey(keyPair);//GetText(keyPair.Private);
+			var publicPem = GetText(keyPair.Public);
+
+			var rsaParameters = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)keyPair.Private);
+
+			var cryptoServiceProvider = new RSACryptoServiceProvider();
+			cryptoServiceProvider.ImportParameters(rsaParameters);
+
+			var publicXml = cryptoServiceProvider.ToXmlString(false);
+			var privateXml = cryptoServiceProvider.ToXmlString(true);
+			return new SignatureInformation
+			{
+				PublicXml = publicXml,
+				PrivateXml = privateXml,
+				PublicPem = publicPem,
+				PrivatePem = privatePem
+			};
+		}
+
+		private static string GetText(AsymmetricKeyParameter key)
+		{
+			var stringWriter = new StringWriter();
+			var pemWriter = new PemWriter(stringWriter);
+
+			pemWriter.WriteObject(key);
+
+			pemWriter.Writer.Flush();
+			return stringWriter.ToString();
+		}
+		private static string GetPrivateKey(AsymmetricCipherKeyPair key)
+		{
+			var stringWriter = new StringWriter();
+			var pemWriter = new PemWriter(stringWriter);
+
+			pemWriter.WriteObject(new Pkcs8Generator(key.Private));
+
+			pemWriter.Writer.Flush();
+			return stringWriter.ToString();
+		}
+	}
+}
